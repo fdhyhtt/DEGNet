f891ea662e83faabfb5bc80ed5fc29bd9ae38ae0
yourbikun <98460564+yourbikun@users.noreply.github.com>
2023-05-07 19:20:16 +0800

    Rename x.sh to run.sh

---
+++ run.sh
@@ -0,0 +1,2 @@
+python tools/train.py --config 'configs/ffm_synins_da.yaml'
+python tools/train.py --config 'configs/ffm_synins_da_pds_cas.yaml'
--- x.sh
+++
@@ -1,2 +0,0 @@
-python tools/train.py --config 'configs/ffm_synins_da.yaml'
-python tools/train.py --config 'configs/ffm_synins_da_pds_cas.yaml'
